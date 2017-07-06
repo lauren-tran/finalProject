c3760d1d86520094002ac7b4405c6eafec034463
Christine <abchristinee@gmail.com>
2017-07-06 12:31:31 -0700

    started game

--- js/preload.js
+++ js/preload.js
@@ -5,3 +5,7 @@ InfiniteScroller.Preload = function(){};
 InfiniteScroller.Preload.prototype = {
   preload: function() {
       this.preload = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preload');
+      this.preload.anchor.setTo(0,5);
+      this.preload.scale.setTo(3);
+      
+//      this.load.pre
