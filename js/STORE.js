4223ab30962f904952e803a7fc8050bd3c4009e5
Lauren <laurentran184@gmail.com>
2017-07-12 09:51:10 -0700

    character store

---
+++ js/STORE.js
@@ -0,0 +1,42 @@
+function gameStore(game) {
+    
+    return {
+        preload: function () {
+            this.game.load.image('shop', 'images/shop.png');
+            
+            this.game.load.image('characterbutton', 'images/characterbutton2.png');
+            
+            this.game.load.image('powerup', 'images/powerup2.png');
+            
+            this.game.load.image('back', 'images/back2.png');
+        },
+        
+        create: function () {
+            this.game.stage.backgroundColor = "#000000";
+            
+            var shop = this.game.add.sprite(400, 150, 'shop');
+            shop.anchor.setTo(0.5, 0.5);
+            
+            var back = this.game.add.button(50, 50, 'back', this.menu, null, this);
+            back.anchor.setTo(0.5, 0.5);
+            
+            var characterbutton = this.game.add.button(400, 340, 'characterbutton', this.characterpage, null, this);
+            characterbutton.anchor.setTo(0.5, 0.5);
+            
+            var powerup = this.game.add.button(400, 475, 'powerup', this.poweruppage, null, this);
+            powerup.anchor.setTo(0.5, 0.5);
+        },
+        
+            menu: function() {
+                this.game.state.start('title');
+        },
+            
+            characterpage: function() {
+                this.game.state.start('characters');
+        },
+        
+            poweruppage: function() {
+                this.game.state.start('powerup');
+        }
+    }
+}
--- js/gameTitle.js
+++ js/gameTitle.js
@@ -1,50 +1,43 @@
 function gameTitle(game) {
     return {
-        preload: function(){
-           game.load.image('shopButton', 'images/shopButton.png');
+        preload: function () {
+            game.load.image('store', 'images/store2.png');
             
-            game.load.image('playButton', 'images/playButton.png');
+            game.load.image('play', 'images/playbutton.png');
             
-            game.load.image('missionsButton', 'images/missionsButton.png');
+            game.load.image('missions', 'images/missions2.png');
             
-            game.load.image('leaderboard', 'images/leaderboard.png');
+            game.load.image('leaderboard', 'images/leader2.png');
         },
         
-        create: function(){
+        create: function () {
             
-            var gameTitle = this.game.add.sprite(385,100,'gameTitle');
+            var gameTitle = this.game.add.sprite(400, 100, 'gameTitle');
+            gameTitle.anchor.setTo(0.5, 0.5);
 
-            gameTitle.anchor.setTo(0.5,0.5);
+            var play = this.game.add.button(168, 300, 'play', this.playTheGame, null, this);
 
-            var playButton=
-                game.add.button(385,250,'playButton',function (){
-                    game.state.start('start')
-                })
-            playButton.anchor.setTo(0.5,0.5);
-
-            playButton.scale.setTo(0.5,0.5);
-
-
-            var shopButton = 
-                game.add.button(385,350,'shopButton',function(){
-                    game.state.start('')
-                })
-            shopButton.scale.setTo(0.5)
-
-            shopButton.anchor.setTo(0.5,0.5);
+            var store = this.game.add.button(168, 450, 'store', this.store, null, this);
             
-            var missionsButton = this.game.add.button(385,450,'missionsButton', this.playTheGame, null, this)
-            missionsButton.anchor.setTo(0.5,0.5);
-            missionsButton.scale.setTo(0.5);
+            var missions = this.game.add.button(350, 450, 'missions', this.missions, null, this);
             
-            var leaderboard = this.game.add.button(385,550,'leaderboard', this.playTheGame,null,this)
-            leaderboard.anchor.setTo(0.5,0.5);
-            leaderboard.scale.setTo(0.5,0.5);
+            var leaderboard = this.game.add.button(532, 450, 'leaderboard', this.leader, null, this);
         },
     
-        playTheGame: function(){
-    
-            this.game.state.start('main');
+        playTheGame: function () {
+            this.game.state.start('start');
+        },
+        
+        store: function () {
+            this.game.state.start('store');
+        },
+        
+        missions: function () {
+            this.game.state.start('missions');
+        },
+        
+        leader: function () {
+            this.game.state.start('leaderboard');
         }
     }
 }
